fb656c0a4a1327264c82a700222a89d96c880b77
Roomdada <dsieroger@gmail.com>
2022-06-11 14:46:29 +0000

    refactor dbseed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Role;
 use App\Models\User;
+use App\Models\Emploi;
 use App\Models\Contrat;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -18,12 +20,45 @@ class DatabaseSeeder extends Seeder
    */
   public function run()
   {
- 
+
+
 
     $this->call([
       ContratsSeeder::class,
       RolesSeeder::class,
-      UsersSeeder::class,
     ]);
+
+    User::create([
+      'nom' => 'Admin',
+      'prenoms' => 'Bara Gnini',
+      'email' => 'admin@email.com',
+      'role_id' => Role::SUPER_ADMIN,
+      'password' => Hash::make('pass2022'),
+    ]);
+
+
+    if (env('APP_ENV') == 'production') {
+      $this->call(UsersSeeder::class);
+      return 1;
+    }
+
+    for ($i = 0; $i < 15; $i++) {
+
+      $user = User::create([
+        'nom' => 'RH',
+        'prenoms' => "Admin ".$i,
+        'email' => "drh$i@bara.ci",
+        'role_id' => Role::DRH,
+        'password' => Hash::make('pass2022'),
+      ]);
+
+      Emploi::create([
+        'titre' => "offfre " . Str::random(3),
+        'description' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut autem amet eligendi exercitationem, animi mollitia? Eligendi expedita veritatis quae in maxime aliquid ducimus, incidunt qui tenetur velit atque laborum ipsum",
+        'disponible' => rand(0, 1),
+        'contrat_id' => \App\Models\Contrat::all()->random()->id,
+        'user_id' => $user->id,
+      ]);
+    }
   }
 }
